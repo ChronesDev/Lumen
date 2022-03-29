d9f8ed6c787e26ab982aec574b61e367b579e577
CXCubeHD <cxcubehd@gmail.com>
2022-03-29 19:34:34 +0200

    started working on render stuff

---
+++ Lumen/lumen/render/render.cc
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <indx>
+
+#include <indxs>
+
+namespace Lumen::Render
+{
+    static bool Initialized_ = false;
+
+    fun Init()->void;
+    fun Deinit()->void;
+}
+
+#include <indxe>
---
+++ Lumen/lumen/render/render.impl.cc
@@ -0,0 +1,6 @@
+#include "render.cc"
+
+namespace Lumen::Render
+{
+
+}
