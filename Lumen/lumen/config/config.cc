002596d0933bff1972606d337622147d5ef42d2c
CXCubeHD <cxcubehd@gmail.com>
2022-06-10 12:40:13 +0200

    fixed config.cc for 1.19.0

--- Lumen/lumen/config/config.cc
+++ Lumen/lumen/config/config.cc
@@ -8,7 +8,7 @@ namespace Lumen::Config
 {
     global var ClientVersion = Version::Parse("0.4");
 
-    global var TargetVersion = Version::Parse("1.19.0");
+    global var TargetVersion = Version::Parse("1.19.0.5");
     global var TargetPackageVersion = Version::Parse("1.19.5.0");
 }
 
